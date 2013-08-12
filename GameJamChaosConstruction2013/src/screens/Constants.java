f6dd75d9dffa230cec24581ea7201c71f2815757
de43gy <de43gy@gmail.com>
2013-08-12 13:07:56 +0400

    a lot of changes, can not list

--- GameJamChaosConstruction2013/src/screens/Constants.java
+++
@@ -1,9 +0,0 @@
-package screens;
-
-public interface Constants {
-	public final int SPASE_WIDTH =300;
-	public final int SPASE_HEIGHT =300;
-	public final int SPACE_SHIP_START_Y =100;
-	public final int SPACE_SHIP_START_X =300;
-
-}
